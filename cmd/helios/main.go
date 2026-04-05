982d6e179419a4c37ddd23eebf8db280798aa49f
Edward Li <jiaxi.edwardli@gmail.com>
2026-04-05 11:10:47 -0700

    Add better exit handling

--- cmd/helios/main.go
+++ cmd/helios/main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-	"os"
-	"sync"
-
+	"fmt"
 	"helios/internal/client"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	var wg sync.WaitGroup
-
 	runtime_hash := os.Getenv("RUNTIME_HASH")
 	docker_disabled := os.Getenv("DOCKER_DISABLED")
 	component_tree_path := os.Getenv("COMPONENT_TREE_PATH")
 
 	runType := "docker"
-	if docker_disabled == "1" { runType = "local" }
+	if docker_disabled == "1" {
+		runType = "local"
+	}
 
 	cli := client.Initialize(runType, runtime_hash)
 	defer cli.Close()
 
 	cli.InitializeComponentTree(component_tree_path)
 	go cli.StartAllComponents()
-	wg.Add(1)
 
-	// Stop the main program from terminating
-	// All code is now running in goroutines
-	wg.Wait()
+	// Wait for Ctrl+C or kill signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit // blocks until Ctrl+C or kill signal
+
+	fmt.Println("Shutting down...")
 }
